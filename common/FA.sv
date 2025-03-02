af0d7904603c62fce00b6c6e42fb3d3a863ed246
IlBiondo97 <biundo.ivan@gmail.com>
2025-03-03 00:28:47 +0100

    chores: added FA for the CSA

---
+++ common/FA.sv
@@ -0,0 +1,19 @@
+/**
+ * @file FA.sv
+ * @brief Full Adder.
+ *
+ * @author Ivan Biundo
+ * @date 01/03/2025
+ */
+
+ module FA (
+    input logic a,
+    input logic b,
+    input logic cin,
+    output logic sum,
+    output logic cout
+ );
+
+ assign sum = a ^ b ^ cin;
+ assign cout = (a & b) | (cin & (a ^ b));
+ endmodule
